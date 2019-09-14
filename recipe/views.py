cade11da9d06b6e0013d47441b074344bf35841e
Gustavo Soeiro Senise <gustavosooeiro@gmail.com>
2019-09-13 22:22:29 -0300

    added funcionality to create tags

--- recipe/views.py
+++ recipe/views.py
@@ -3,11 +3,18 @@ from rest_framework import viewsets, mixins
 from .models import Tag
 from .serializers import TagSerializer
 
-class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
+class TagViewSet(viewsets.GenericViewSet, 
+                 mixins.ListModelMixin,
+                 mixins.CreateModelMixin):
     """Manage tags in the database"""
 
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
 
     def get_queryset(self):
-        self.queryset.order_by('-name')
+        """Return objects ordered by name"""
+        return self.queryset.all().order_by('name')
+
+    def perform_create(self, serializer):
+        """Create a new tag"""
+        serializer.save()
